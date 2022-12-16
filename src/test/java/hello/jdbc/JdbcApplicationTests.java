e0cc683189eda7f2340ec9ee8c64669e457e3772
SwAurora <rlatjddnr74@gmail.com>
2022-12-16 16:14:09 +0900

    2022-12-16 master(ver1)

---
+++ src/test/java/hello/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,15 @@
+package hello.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
